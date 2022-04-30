8e56347b5ac8f9d1aabf8759863fc184b32a0674
Flavia Santos <flaviafeernanda@gmail.com>
2022-04-30 12:25:51 -0300

    cenários desenvolvidos na aula 2 de Cypress

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,12 +1,40 @@
-/// <reference types="cypress" />
+/// <reference types="cypress"/>
+/// <reference types="cypress-xpath" />
 
-context('Funcionalidade Login', () =>{
-    it('Deve fazer login com sucesso',() =>{
+context('Funcionalidade Login', () => {
+
+    beforeEach(() => {
         cy.visit('/')
-        cy.xpath('\\').click()
     })
 
-    it('Deve exibir mensagem de erro ao inserir usuário ou senha incorretos', () =>{
+    it('Deve fazer login com sucesso', () => {
 
+        cy.xpath('//i[contains(@class,"unfollow")]').click()
+        cy.get('#username').type('aluno_ebac@teste.com')
+        cy.get('#password').type('teste@teste.com')
+        cy.xpath('//form[contains(@class,"woocommerce-form-login")]/input[@type="submit"]').click()
+
+        cy.xpath('//h1[@class="page-title"]').should('contain', 'Minha conta')
+        cy.xpath('//div[@class="woocommerce-MyAccount-content"]').should('contain', 'Olá, aluno_ebac')
+    })
+
+    it.only('Deve exibir mensagem de erro ao inserir usuário inválido', () => {
+
+        cy.xpath('//i[contains(@class,"unfollow")]').click()
+        cy.get('#username').type('alunoebac@teste.com')
+        cy.get('#password').type('teste@teste.com')
+        cy.xpath('//form[contains(@class,"woocommerce-form-login")]/input[@type="submit"]').click()
+        
+        cy.get('.woocommerce-error').should('contain', 'Endereço de e-mail desconhecido. Verifique novamente ou tente seu nome de usuário')
+    })
+
+    it.only('Deve exibir mensagem de erro ao inserir senha inválida', () => {
+
+        cy.xpath('//i[contains(@class,"unfollow")]').click()
+        cy.get('#username').type('aluno_ebac@teste.com')
+        cy.get('#password').type('testetestecom')
+        cy.xpath('//form[contains(@class,"woocommerce-form-login")]/input[@type="submit"]').click()
+
+        cy.get('.woocommerce-error').should('contain', 'Erro: A senha fornecida para o e-mail aluno_ebac@teste.com está incorreta. Perdeu a senha?')
     })
 })
