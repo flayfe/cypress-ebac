0f35e3d5ed0f282e6c35fd08086c54e0879efbb5
Flavia Santos <flaviafeernanda@gmail.com>
2022-05-03 19:44:17 -0300

    Usando massa de dados para o teste de login

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,6 +1,8 @@
 /// <reference types="cypress"/>
 /// <reference types="cypress-xpath" />
 
+const perfil = require('../fixtures/perfil.json')
+
 context('Funcionalidade Login', () => {
 
     beforeEach(() => {
@@ -18,13 +20,36 @@ context('Funcionalidade Login', () => {
         cy.xpath('//div[@class="woocommerce-MyAccount-content"]').should('contain', 'Olá, aluno_ebac')
     })
 
+    it('Deve fazer login com sucesso - Usando arquivo de dados', () => {
+
+        cy.xpath('//i[contains(@class,"unfollow")]').click()
+        cy.get('#username').type(perfil.usuário)
+        cy.get('#password').type(perfil.senha)
+        cy.xpath('//form[contains(@class,"woocommerce-form-login")]/input[@type="submit"]').click()
+
+        cy.xpath('//h1[@class="page-title"]').should('contain', 'Minha conta')
+        cy.xpath('//div[@class="woocommerce-MyAccount-content"]').should('contain', 'Olá, aluno_ebac')
+    })
+
+    it.only('Deve fazer login com sucesso - Usando fixture', () => {
+        cy.fixture('perfil').then(dados => {
+            cy.xpath('//i[contains(@class,"unfollow")]').click()
+            cy.get('#username').type(dados.usuário)
+            cy.get('#password').type(dados.senha, {log: false})
+            cy.xpath('//form[contains(@class,"woocommerce-form-login")]/input[@type="submit"]').click()
+
+            cy.xpath('//h1[@class="page-title"]').should('contain', 'Minha conta')
+            cy.xpath('//div[@class="woocommerce-MyAccount-content"]').should('contain', 'Olá, aluno_ebac')
+        })
+    })
+
     it('Deve exibir mensagem de erro ao inserir usuário inválido', () => {
 
         cy.xpath('//i[contains(@class,"unfollow")]').click()
         cy.get('#username').type('alunoebac@teste.com')
         cy.get('#password').type('teste@teste.com')
         cy.xpath('//form[contains(@class,"woocommerce-form-login")]/input[@type="submit"]').click()
-        
+
         cy.get('.woocommerce-error').should('contain', 'Endereço de e-mail desconhecido. Verifique novamente ou tente seu nome de usuário')
     })
 
